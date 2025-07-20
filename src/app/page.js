90d5bbcb13f9c7363cca5f5f0fb7ffb5f4437217
tarunmalik330 <maliktarun2005@gmail.com>
2025-07-20 20:19:36 +0530

    remove unused ViewAppointments component and AppointmentsContext

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,14 @@
 import Header from "@/components/common/Header";
 import Services from "@/components/home/Services";
-// import ViewAppointments from "@/components/home/ViewAppointments";
-// import { AppointmentsProvider } from "@/context/AppointmentsContext";
-// import ViewAppointments from "@/app/ViewAppointments"; // If you have this file
+import React from "react";
 
-export default function Home() {
+const page = () => {
   return (
-    // <AppointmentsProvider>
-    <>
+    <div>
       <Header />
       <Services />
-    </>
-    /* <ViewAppointments />
-    </AppointmentsProvider> */
+    </div>
   );
-}
+};
+
+export default page;
