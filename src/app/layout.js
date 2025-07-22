87a37d69c4c6f5492bcce2c6c79fb23c262ed742
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2025-07-22 10:00:43 +0530

    merge with dev

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppointmentProvider } from "@/context/AppointmentsContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className=""
       >
         <AppointmentProvider>{children}</AppointmentProvider>
       </body>
