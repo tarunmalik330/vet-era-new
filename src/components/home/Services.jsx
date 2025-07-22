ea40038029d677e627836fd92f3c4db01c0ef930
tarunmalik330 <maliktarun2005@gmail.com>
2025-07-22 20:53:13 +0530

    change btn hover

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -16,13 +16,13 @@ const Services = () => {
         <div className="flex gap-4 md:mb-12 mb-8 justify-center flex-wrap">
           <Link
             href="/appointments/schedule"
-            className="bg-blue-950 text-white hover:text-black cursor-pointer px-6 py-2 rounded hover:bg-transparent transition border border-transparent hover:border-blue-950 text-sm md:text-base sm:py-1 duration-300"
+            className="bg-blue-950 text-white font-titillium hover:text-black cursor-pointer px-6 py-2 rounded hover:bg-transparent transition border border-transparent hover:border-blue-950 text-sm md:text-base sm:py-1 duration-300"
           >
             Schedule Appointment
           </Link>
           <Link
             href="/appointments/view"
-            className="bg-white text-black hover:text-white px-6 py-2 rounded cursor-pointer hover:bg-blue-950 transition text-sm md:text-base sm:py-1 duration-300"
+            className="bg-white text-black font-titillium hover:text-white px-6 py-2 rounded cursor-pointer hover:bg-blue-950 transition text-sm md:text-base sm:py-1 duration-300"
           >
             View Appointments
           </Link>
